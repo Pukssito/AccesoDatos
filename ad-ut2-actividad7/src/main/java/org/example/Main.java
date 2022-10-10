af9977d4546c1d6732e8c3b46167ceaf58bd81fe
GabrielC <96666808+Pukssito@users.noreply.github.com>
2022-10-10 14:14:02 +0200

    ejercicio 8 sin acabar

--- ad-ut2-actividad7/src/main/java/org/example/Main.java
+++ ad-ut2-actividad7/src/main/java/org/example/Main.java
@@ -4,21 +4,23 @@ package org.example;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
 
-
     public static void main(String[] args) {
         File archivo = new File(args[0]);
+        File file = new File(args[0] + "/ficheros.txt");
+        File directory = new File(args[0] + "/directorios.txt");
         File[] pathnames = archivo.listFiles();
-        FileWriter fichero = null;
+        FileWriter fileW = null;
         PrintWriter pw;
 
         try {
-            fichero = new FileWriter(args[0] + "/Ejercicio7.txt");
-            pw = new PrintWriter(fichero);
+            fileW = new FileWriter(args[0] + "/Ejercicio7.txt");
+            pw = new PrintWriter(fileW);
             for (File pathname : pathnames) {
                 pw.write(pathname.getName());
                 pw.write(";");
@@ -48,9 +50,38 @@ public class Main {
             e.printStackTrace();
         }
         try {
-            fichero.close();
+            fileW.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        File ficheroEjercicio7 = new File(args[1]);
+        FileReader fr;
+        try {
+            fr = new FileReader(ficheroEjercicio7);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        BufferedReader br = new BufferedReader(fr);
+        String linea;
+        int count = 0;
+        while (true) {
+            try {
+                if (!((linea = br.readLine()) != null)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(linea);
+            if (linea.contains(";F;")) {
+                try {
+                    pw = new PrintWriter(file);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
+        }
+        System.out.println(count + " Ficheros");
     }
 }
